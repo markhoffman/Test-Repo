7d7d5543da4c5d8228cd3c7b47f16a3e1977c7ee
markhoffman <mark.hoffman@quinnipiac.edu>
2014-06-02 16:24:45 -0400

    8th commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,8 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println ("Hello Kitty");
+		
+		System.out.println ("Hello Kitty Cat");
 
 	}
 
